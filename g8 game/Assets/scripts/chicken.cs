ae80b4df594d9f01237d15c68fd61e860432f379
kiki <ccnk3847@gmail.com>
2019-10-30 16:39:58 +0800

    G8雞動作完成

---
+++ g8 game/Assets/scripts/chicken.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;//引用Unity API
+
+//類別 類別名稱
+public class chicken : MonoBehaviour
+{
+  
+    //宣告變數(定義欄位 Field)
+    //欄位類型 欄位名稱 指定 值 結束
+    int speed = 10;          // 整數1, 9999, -100
+    float turn = 20.5f;      //浮點數
+    bool mission;            //布林值 true false
+    string Name = "G8 chicken";     // 字串 ""
+}
