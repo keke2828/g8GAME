837b09275b6213abae16eb72daccabd85da56d34
kiki <ccnk3847@gmail.com>
2019-12-30 23:35:53 +0800

    11.20-3
    11.20-3

---
+++ g8 game/Assets/scripts/LearnMethod.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // 定義方法
+    //語法:
+    //修飾詞 傳回類型 方法名稱 () { 陳敘事或演算法}
+    //void 無傳回
+    private void Drive()
+    {
+        //輸出訊息(字串);
+        print("開車,時速:200")
+    }
+
+
+}
